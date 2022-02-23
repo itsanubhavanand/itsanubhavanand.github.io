eb3a8813725da5cc5295c3f7b9d92f212ac21705
Anubhav Anand Pandey <73177615+itsanubhavanand@users.noreply.github.com>
2022-02-23 15:53:08 +0530

    Updating Employee details
    Updating Employee details

--- data1.js
+++ data1.js
@@ -12,7 +12,7 @@ function showEmp1(){
 	strEmployeeDataTable = strEmployeeDataTable + '</thead>';
 	strEmployeeDataTable = strEmployeeDataTable + '<tboday>';
 	for(var i=0; i<employeeData.length; i++) {
-	strEmployeeDataTable = strEmployeeDataTable + '<tr> <th scope="row">'+(j)+'</th> <th scope="row">'+(i+101)+'</th> <td>'+employeeData[i].First+'</td> <td>'+employeeData[i].Place+'</td> <td>'+employeeData[i].State+'</td> </tr>' ;
+	strEmployeeDataTable = strEmployeeDataTable + '<tr> <th scope="row">'+(j)+'</th> <th scope="row">'+employeeData[i].EmpId+'</th> <td>'+employeeData[i].First+'</td> <td>'+employeeData[i].Place+'</td> <td>'+employeeData[i].State+'</td> </tr>' ;
      j=j+1;
     }
 	 
@@ -33,7 +33,7 @@ function showEmp2(){
 	strEmployeeDataTable = strEmployeeDataTable + '</thead>';
 	strEmployeeDataTable = strEmployeeDataTable + '<tboday>';
 	for(var i=0; i<employeeData.length; i=i+2) {
-	strEmployeeDataTable = strEmployeeDataTable + '<tr> <th scope="row">'+(j)+'</th> <th scope="row">'+(i+102)+'</th> <td>'+employeeData[i+1].First+'</td> <td>'+employeeData[i+1].Place+'</td> </tr>' ;
+	strEmployeeDataTable = strEmployeeDataTable + '<tr> <th scope="row">'+(j)+'</th> <th scope="row">'+employeeData[i+1].EmpId+'</th> <td>'+employeeData[i+1].First+'</td> <td>'+employeeData[i+1].Place+'</td> </tr>' ;
      j=j+1;
     }
 	  
@@ -53,7 +53,7 @@ function showEmp3(){
 	strEmployeeDataTable = strEmployeeDataTable + '</thead>';
 	strEmployeeDataTable = strEmployeeDataTable + '<tboday>';
 	for(var i=0; i<employeeData.length; i=i+2) {
-	strEmployeeDataTable = strEmployeeDataTable + '<tr> <th scope="row">'+(j)+'</th> <th scope="row">'+(i+101)+'</th> <td>'+employeeData[i].First+'</td> <td>'+employeeData[i].Place+'</td> </tr>' ;
+	strEmployeeDataTable = strEmployeeDataTable + '<tr> <th scope="row">'+(j)+'</th> <th scope="row">'+employeeData[i].EmpId+'</th> <td>'+employeeData[i].First+'</td> <td>'+employeeData[i].Place+'</td> </tr>' ;
      j=j+1;
     }
 	  
@@ -71,66 +71,93 @@ function clear1(){
 }
 
 function fiind(){
-		var s = document.getElementById('form1').value;
-		Console.log(s);
+		var s = document.getElementById('num1').value;
+		var strEmployeeDataTable= "";
+	strEmployeeDataTable = '<table class="table table-striped table-hover">';
+	strEmployeeDataTable = strEmployeeDataTable + '<thead>';
+	strEmployeeDataTable = strEmployeeDataTable + '<tr> <th scope="col">#</th> <th scope="col">EmpId</th> <th scope="col">First</th>  <th scope="col">Place</th> <th scope="col">State</th> </tr>';
+	strEmployeeDataTable = strEmployeeDataTable + '</thead>';
+	strEmployeeDataTable = strEmployeeDataTable + '<tboday>';
+		for( var i=0; i<employeeData.length; i++){
+
+			if(employeeData[i].EmpId == s){
+				strEmployeeDataTable = strEmployeeDataTable + '<tr> <th scope="row">'+(i+1)+'</th> <th scope="row">'+employeeData[i].EmpId+'</th> <td>'+employeeData[i].First+'</td> <td>'+employeeData[i].Place+'</td> <td>'+employeeData[i].State+'</td> </tr>' ;
+				break;
+			}
+		}strEmployeeDataTable = strEmployeeDataTable + '</tbody>';
+	strEmployeeDataTable = strEmployeeDataTable + '</table>' ;
+	document.getElementById("place").innerHTML = strEmployeeDataTable ;
+		
+		
 }
 
 
 employeeData = [{
+  "EmpId" : "101",
   "First": "Ankit",
   "Last" :"Gupta",
   "Place" : "Firojabad",
   "State" : "UP"
+  
 },
 {
+	"EmpId" : "102",
   "First": "Jothilakshami",
   "Last" :"V",
   "Place" : "Rajapalayam",
   "State" : "TN"
 },
 {
+	"EmpId" : "103",
   "First": "Avinash",
   "Last" :"V",
   "Place" : "Kushinagar",
   "State" : "UP"
 },
 {
+  "EmpId" : "104",
   "First": "Vikash",
   "Last" :"V",
   "Place" : "Deoria",
   "State" : "UP"
 },
 {
+	"EmpId" : "105",
   "First": "Rakesh",
   "Last" :"V",
   "Place" : "Bangalore",
   "State" : "kARNATAKA"
 },
 {
+  "EmpId" : "106",
   "First": "Vishakha",
   "Last" :"V",
   "Place" : "Rajapalayam",
   "State" : "TN"
 },
 {
+  "EmpId" : "107",
   "First": "Ravi",
   "Last" :"V",
   "Place" : "Karolbagh",
   "State" : "Delhi"
 },
 {
+  "EmpId" : "108",
   "First": "Jothilakshami",
   "Last" :"V",
   "Place" : "Rajapalayam",
   "State" : "TN"
 },
 {
+  "EmpId" : "109",
   "First": "Vinay",
   "Last" :"V",
   "Place" : "Pune",
   "State" : "Maharashtra"
 },
 {
+  "EmpId" : "110",
   "First": "Dravid",
   "Last" :"V",
   "Place" : "Bangalore",
